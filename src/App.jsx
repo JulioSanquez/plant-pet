d695c40798a6dc6e302ff8aefd63c978a8f6c2d4
Julio Sanquez <julio930911@gmail.com>
2023-01-01 18:50:21 -0600

    v1.0.6

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import './App.css'
-import {google} from 'googleapis'
 import { useEffect } from 'react';
 import axios from 'axios';
 
@@ -9,56 +8,11 @@ function App() {
     console.log("Hola Sheets")
   }  
 
-const sheets = google.sheets('v4');
-
-
-  async function authorize() {
-    // TODO: Change placeholder below to generate authentication credentials. See
-    // https://developers.google.com/sheets/quickstart/nodejs#step_3_set_up_the_sample
-    //
-    // Authorize using one of the following scopes:
-    //   'https://www.googleapis.com/auth/drive'
-    //   'https://www.googleapis.com/auth/drive.file'
-    //   'https://www.googleapis.com/auth/drive.readonly'
-    //   'https://www.googleapis.com/auth/spreadsheets'
-    //   'https://www.googleapis.com/auth/spreadsheets.readonly'
-    let authClient = "https://www.googleapis.com/auth/drive.readonly";
-
-    if (authClient == null) {
-      throw Error('authentication failed');
-    }
-
-    return authClient;
-  }
-
   useEffect(() => {
-    async function main () {
-      const authClient = await authorize();
-      const request = {
-        // The ID of the spreadsheet to retrieve data from.
-        spreadsheetId: '1ltPRJdt_KdFI5tuHmfXMvq571Xx2FQAdbOCLvN-jvgc',  // TODO: Update placeholder value.
-  
-        // The A1 notation of the values to retrieve.
-        range: 'prueba',  // TODO: Update placeholder value.
-  
-        // How values should be represented in the output.
-        // The default render option is ValueRenderOption.FORMATTED_VALUE.
-        valueRenderOption: 'FORMATTED_VALUE',  // TODO: Update placeholder value.
-  
-        // How dates, times, and durations should be represented in the output.
-        // This is ignored if value_render_option is
-        // FORMATTED_VALUE.
-        // The default dateTime render option is [DateTimeRenderOption.SERIAL_NUMBER].
-        dateTimeRenderOption: 'SERIAL_NUMBER',  // TODO: Update placeholder value.
-  
-        auth: authClient,
-      };
-  
-      axios.get(sheets.spreadsheets.values.get(request))
+    
+      axios.get("https://sheet.best/api/sheets/b84ff152-9edf-4731-9241-73beccd90047")
         .then( ({data}) => console.log(data) )
         .catch( err => {console.log(data); console.log("Salio un Error Carnal")} )
-    }
-    main();
   }, [])
   
 
